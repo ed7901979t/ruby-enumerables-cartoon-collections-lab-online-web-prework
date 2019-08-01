793f26d285503a19e7226c9e21debb61e433746e
clean-command-1851 <ed7901979t@gmail.com>
2019-08-01 19:56:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(array)
   # Your code here
   array.map { |array| array.capitalize }
-  puts ""
+  puts "#{array}!"
 end
 
 def long_planeteer_calls# code an argument here
